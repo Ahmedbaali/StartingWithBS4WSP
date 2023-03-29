7acc852afc0bbb2359d97a8988b19d1c3ef35014
Ahmed BAALI "DSI <Ahmed.bl@Techno-dz.com>
2023-03-29 16:00:54 +0100

    changing html file

--- FirstF/SecondStep.py
+++ FirstF/SecondStep.py
@@ -9,3 +9,13 @@ for tag in tags:
     print(tag.strip())
 tagss = list(map(lambda x: x.strip(), tags))
 print(tagss)
+
+# saving a new html file
+
+tags2 = doc.find_all("input", type="text", limit=2)
+for t in tags2:
+    t['placeholder'] = "it is changed!!"
+
+with open("changed.html", "w") as file:
+    file.write(str(doc))
+print("done!!!!!!")
